1e468184d8255ddef1848f95b0f2285bfe5e631b
Robert Grochowicz <robert.grochowicz@gmail.com>
2013-01-09 01:34:03 -0500

    Changed FAQ to accordion

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,8 @@
 $(function() {
 	$("body").scrollspy();
+
+	$(".faq-answer").collapse({toggle:true, parent: "#faq-acc"});
+	$(".faq-header").click(function() {
+		$(this).parents(".accordion-group").find(".faq-answer").collapse('toggle');
+	});
 });
